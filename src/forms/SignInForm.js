a6cdf8299c06d5a5bfc0cba7d082b642f10328ad
Addison Moore <addisondrewmoore@gmail.com>
2019-01-31 01:41:35 -0800

    Password validation and show/hide password

--- src/forms/SignInForm.js
+++ src/forms/SignInForm.js
@@ -3,25 +3,45 @@
 import React, { Component } from "react";
 import { KeyboardAvoidingView, StyleSheet, View } from "react-native";
 import { Button, TextInput, HelperText } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 class SignInForm extends Component {
   state = {
     text: "",
     username: "",
     outlinedText: "",
-    password: ""
+    password: null,
+    icEye: "visibility-off",
+    passwordHidden: true,
+    validPassword: true
   };
 
   componentWillUnmount() {
     this.setState({ text: "", name: "", outlinedText: "" });
   }
 
+  handleChangePassword = password => {
+    this.setState({ password });
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{7,})/.test(this.state.password)
+      ? this.setState({ validPassword: true })
+      : this.setState({ validPassword: false });
+  };
+
   _isUsernameValid = () => /^[a-zA-Z0-9]*$/.test(this.state.username);
 
-  _isPasswordValid = () =>
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})$/.test(
-      this.state.password
-    );
+  hideShowPassword = () => {
+    if (this.state.passwordHidden) {
+      this.setState({
+        icEye: "visibility",
+        passwordHidden: false
+      });
+    } else {
+      this.setState({
+        icEye: "visibility-off",
+        passwordHidden: true
+      });
+    }
+  };
 
   render() {
     return (
@@ -34,6 +54,7 @@ class SignInForm extends Component {
           <TextInput
             mode="outlined"
             label="USERNAME"
+            onChange={this._isPasswordValid}
             value={this.state.username}
             error={!this._isUsernameValid()}
             onChangeText={username => this.setState({ username })}
@@ -44,11 +65,19 @@ class SignInForm extends Component {
         </View>
         <View style={styles.inputContainerStyle}>
           <TextInput
+            secureTextEntry={this.state.passwordHidden}
             mode="outlined"
-            label="Password"
-            value={this.state.username}
-            error={!this._isPasswordValid()}
-            onChangeText={password => this.setState({ password })}
+            label="PASSWORD"
+            value={this.state.password}
+            error={!this.state.validPassword}
+            onChangeText={password => this.handleChangePassword(password)}
+          />
+          <Icon
+            style={styles.icon}
+            name={this.state.icEye}
+            size={24}
+            color={"#000"}
+            onPress={this.hideShowPassword}
           />
           <HelperText type="error" visible={!this._isUsernameValid()}>
             Error: Only letters and numbers are allowed
@@ -62,4 +91,10 @@ class SignInForm extends Component {
 
 export default SignInForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  icon: {
+    position: "absolute",
+    right: 12,
+    top: 24
+  }
+});
